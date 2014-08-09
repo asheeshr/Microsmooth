911bc9eed8ef70ddbee9e3204be142814c2faa25
Asheesh <asheeshr@users.noreply.github.com>
2014-08-09 12:55:56 +0530

    Added license to files

--- microsmooth.cpp
+++ microsmooth.cpp
@@ -1,3 +1,15 @@
+/*
+Microsmooth, DSP library for Arduino
+Copyright (C) 2013, Asheesh Ranjan, Pranav Jetley
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
 #include "microsmooth.h"
 #include <Arduino.h>
 //#include <Serial.h>
